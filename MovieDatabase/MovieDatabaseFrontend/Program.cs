e08bbbc898f9c5ef5035716e5ab92b399522bc11
DanielsPc\Daniel <d.breul@gmx.de>
2026-01-01 17:43:09 +0100

    Frontend: Finish layout

--- MovieDatabase/MovieDatabaseFrontend/Program.cs
+++ MovieDatabase/MovieDatabaseFrontend/Program.cs
@@ -1,5 +1,5 @@
 using Blazorise;
-using Blazorise.Bootstrap;
+using Blazorise.Bootstrap5;
 using Blazorise.Icons.FontAwesome;
 using MovieDatabaseFrontend.Components;
 using MovieDatabaseFrontend.Services;
@@ -15,7 +15,7 @@ builder.Services
     {
         options.Immediate = true;
     })
-    .AddBootstrapProviders()
+    .AddBootstrap5Providers()
     .AddFontAwesomeIcons();
 
 builder.Services.AddHttpClient();
