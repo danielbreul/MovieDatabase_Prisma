59b1a4fd6fdb3257a1366abc4eeebc4cb01d28b4
DanielsPc\Daniel <d.breul@gmx.de>
2026-01-05 01:38:06 +0100

    Frontend: Global BaseUri; Add related movies modal;

--- MovieDatabase/MovieDatabaseFrontend/Services/IPersonService.cs
+++ MovieDatabase/MovieDatabaseFrontend/Services/IPersonService.cs
@@ -8,5 +8,9 @@ namespace MovieDatabaseFrontend.Services
         public Task<PersonViewModel?> CreatePersonAsync(PersonViewModel genre);
         public Task<bool> UpdatePersonAsync(PersonViewModel genre);
         public Task<bool> DeletePersonAsync(PersonViewModel genre);
+        public Task<IEnumerable<MovieViewModel>> GetDirectedMoviesAsync(int id);
+        public Task<IEnumerable<MovieViewModel>> GetWrittenMoviesAsync(int id);
+        public Task<IEnumerable<MovieViewModel>> GetActedInMoviesAsync(int id);
+
     }
 }
